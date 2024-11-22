01429708b16b554d86eac8792ff093c465114001
Shiv Patel <shivpatel0204@gmail.com>
2024-11-22 14:00:16 -0500

    .

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -10,21 +10,21 @@ function TourGallery() {
     useEffect( () => {
         fetch('https://course-api.com/react-tours-project')
 
-           // .then((response) => {
-             //   if (!response.ok) {
-             //       throw new Error("Failed to fetch data");}
-              //  return response.json();
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Failed to fetch data");}
+                return response.json();
 
-           // })  
+            })  
             .then(data => {
-                setTours(JSON.parse(data.contents)); //updates State with data
+                setTours(data); //updates State with data
                 setLoading(false); //stops loading after data fetched
             })
-            .catch((err) => {
-                setError(err.message); // updates error State if failed fetch
+            .catch((error) => {
+                setError(error.message); // updates error State if failed fetch
                 setLoading(false); // once again will stop loading if error
             });
-}, []);
+}, []); //Dependancy Array
 
 //  Loading & Error Messsages to update user.
 if (loading) return <p>Please Wait, Loading...</p>; 
@@ -33,16 +33,12 @@ if (error) return <p>Error, Failed!</p>;
 
 return (
     <div>
-        <h2>Tour List</h2>
+        <h2>Tour Packages</h2>
         <ul>
             {tours.map((tour) => (
-                <li key={tour.id} style={{ marginBottom: "20px", listStyle: "none" }}>
+                <li key={tour.id}>
                     {/* Display tour images */}
-                    <img
-                        src={tour.image}
-                        alt={tour.name}
-                        style={{ width: "200px", height: "150px", objectFit: "cover" }}
-                    />
+                    <img src={tour.image} alt={tour.name} style={{ width: "200px", height: "150px" }} />
                 <h3>{tour.name}</h3>
                     <p><strong>Price:</strong> ${tour.price}</p>
                     <p>{tour.info}</p>
